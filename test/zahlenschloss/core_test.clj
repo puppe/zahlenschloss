23a12d09dcb7bc3b90893630607c3eb129d7cf4c
Martin Puppe <mp@mpuppe.de>
2016-03-28 19:59:51 +0200

    Initial commit

---
+++ test/zahlenschloss/core_test.clj
@@ -0,0 +1,7 @@
+(ns zahlenschloss.core-test
+  (:require [clojure.test :refer :all]
+            [zahlenschloss.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
