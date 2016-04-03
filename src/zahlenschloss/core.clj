1c9b643be60c0ebacf846b27accdc453ce30e45f
Martin Puppe <mp@mpuppe.de>
2016-04-03 19:48:03 +0200

    Vollständige Lösung

--- src/zahlenschloss/core.clj
+++ src/zahlenschloss/core.clj
@@ -18,7 +18,104 @@
 (ns zahlenschloss.core
   (:gen-class))
 
+(defn teilbar?
+  "Gibt `true` zurück, genau dann wenn `n` durch `x` teilbar ist."
+  [x n]
+  (= 0 (mod n x)))
+
+(doseq [x [4 7 11]]
+  (intern *ns*
+          (symbol (str "teilbar" x "?"))
+          (partial teilbar? x)))
+
+
+(defn ziffern
+  "Nimmt eine Zahl `n` und gibt die Folge der Ziffern der Zahl zurück."
+  [n]
+  (loop [n n
+         ziffern (list)]
+    (if (= n 0)
+      ziffern
+      (recur (int (/ n 10)) (conj ziffern (mod n 10))))))
+
+; http://stackoverflow.com/a/5058544
+(defn- exp [x n]
+  "Berechnet x hoch n."
+  (reduce * (repeat n x)))
+
+(defn wert
+  [ziffern]
+  "Nimmt eine Folge von Ziffern und berechnet ihren Zahlenwert."
+  (loop [summe 0
+         faktor (exp 10 (dec (count ziffern)))
+         [ziffer & ziffern] ziffern]
+    (if ziffer
+      (recur
+        (+ summe (* ziffer faktor))
+        (/ faktor 10)
+        ziffern)
+      summe)))
+
+(defn links-auffüllen
+  "Nimmt eine Folge von Ziffern und füllt sie, falls nötig, links mit
+  Nullen auf, sodass das Ergebnis aus mindestens sechs Ziffern besteht."
+  [ziffern]
+  (loop [länge (count ziffern)
+         ziffern ziffern]
+    (if (>= länge 6)
+      ziffern
+      (recur
+        (inc länge)
+        (conj ziffern 0)))))
+
+(defn rückwärts
+  "Nimmt eine Zahl und dreht die Folge ihrer Ziffern um. Dabei wird
+  angenommen, dass die Zahl mindestens sechsstellig ist. Das Ergebnis
+  ist daher mindestens auch sechsstellig."
+  [n]
+  (wert (reverse (links-auffüllen (ziffern n)))))
+
+(defn quersumme
+  "Berechnet die Quersumme der Zahl `n`."
+  [n]
+  (apply + (ziffern n)))
+
+(defn zähle-ziffern
+  "Zählt, wie oft die einzelnen Ziffern in der Zahl `n` vorkommen. Es
+  wird angenommen, dass `n` mindestens sechsstellig ist."
+  [n]
+  (reduce
+    (fn [m ziffer]
+      (assoc m ziffer (inc (get m ziffer 0))))
+    {}
+    (links-auffüllen (ziffern n))))
+
+(defn doppelt-oder-gar-nicht?
+  "Gibt `true` zurück, genau dann wenn jede Ziffer in der Zahl doppelt
+  oder gar nicht vorkommt. Es wird angenommen, dass `n` mindestens
+  sechsstellig ist."
+  [n]
+  (if (some #(not= % 2) (vals (zähle-ziffern n)))
+    false
+    true))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Mögliche Lösung(en):")
+  (apply
+    println
+    (->> (range 0 1000000)
+         ; Die Zahl ist durch 4 teilbar.
+         (filter teilbar4?)
+         ; Die Zahl ist rückwärts ebenfalls durch 4 teilbar.
+         (filter #(teilbar4? (rückwärts %)))
+         ; Die Zahl ist durch 7 teilbar.
+         (filter teilbar7?)
+         ; Die Zahl ist nicht durch 11 teilbar.
+         (filter (complement teilbar11?))
+         ; Die Quersumme der Zahl beträgt 32.
+         (filter #(= 32 (quersumme %)))
+         ; Jede Ziffer kommt entweder doppelt oder gar nicht vor.
+         (filter doppelt-oder-gar-nicht?)
+         ; Wir geben maximal 10 Zahlen aus.
+         (take 10))))
--- test/zahlenschloss/core_test.clj
+++ test/zahlenschloss/core_test.clj
@@ -19,6 +19,36 @@
   (:require [clojure.test :refer :all]
             [zahlenschloss.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest teilbar?-test
+  (is (teilbar? 4 16))
+  (is (not (teilbar? 4 15))))
+
+(deftest ziffern-test
+  (is (= (seq (ziffern 123456)) (seq [1 2 3 4 5 6])))
+  (is (= (seq (ziffern 0)) (seq []))))
+
+(deftest wert-test
+  (is (= (wert nil) 0))
+  (is (= (wert '(1 2 3 4 5 6)) 123456)))
+
+(deftest links-auffüllen-test
+  (is (= (seq (links-auffüllen nil)) (seq [0 0 0 0 0 0])))
+  (is (= (seq (links-auffüllen '(1))) (seq [0 0 0 0 0 1]))))
+
+(deftest rückwärts-test
+  (is (= (rückwärts 0) 0))
+  (is (= (rückwärts 1) 100000))
+  (is (= (rückwärts 123456) 654321)))
+
+(deftest quersumme-test
+  (is (= (quersumme 0) 0))
+  (is (= (quersumme 123456) 21)))
+
+(deftest zähle-ziffern-test
+  (is (= (zähle-ziffern 0) {0 6}))
+  (is (= (zähle-ziffern 933) {0 3, 9 1, 3 2})))
+
+(deftest doppelt-oder-gar-nicht?-test
+  (is (not (doppelt-oder-gar-nicht? 0)))
+  (is (not (doppelt-oder-gar-nicht? 12345)))
+  (is (doppelt-oder-gar-nicht? 234234)))
