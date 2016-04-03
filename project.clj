a4e36ee9107a827aa36e7d99c576eef409ea92ff
Martin Puppe <mp@mpuppe.de>
2016-04-03 20:55:36 +0200

    Überarbeite project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject zahlenschloss "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject zahlenschloss "1.0.0"
+  :description "Lösung für die Logelei aus ZEITmagazin 14/2016"
+  :url "https://github.com/puppe/zahlenschloss"
   :license {:name "GNU General Public License Version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
